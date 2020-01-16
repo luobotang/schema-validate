ad1a6ca38b3f3ce9c0becd53de0c9f69b05b4034
汤康兴 <hztangkangxing@corp.netease.com>
2020-01-16 19:43:51 +0800

    更新

--- schema-validate.js
+++ schema-validate.js
@@ -1,4 +1,4 @@
-/* @luobotang/schema-validate v0.1.2 */
+/* @luobotang/schema-validate v0.1.5 */
 
 var SchemaValidate = (function (exports) {
   'use strict';
@@ -21,7 +21,7 @@ var SchemaValidate = (function (exports) {
     return !isEmpty(value)
   }
 
-  function createValidator(data) {
+  function createValidator(data, desc) {
     return function (value, rules) {
       for (var i = 0; i < rules.length; i += 1) {
         var ref = rules[i];
@@ -30,7 +30,7 @@ var SchemaValidate = (function (exports) {
         var checkResult = onValid(value, data);
 
         if (checkResult === false) {
-          return { hasError: true, errorMessage: errorMessage }
+          return { hasError: true, errorMessage: desc + errorMessage } // "数据描述" + "规则错误描述"
         } else if (typeof checkResult === 'object' && checkResult.hasError) {
           return checkResult
         }
@@ -40,21 +40,22 @@ var SchemaValidate = (function (exports) {
     }
   }
 
-  var Type = function Type(name) {
+  var Type = function Type(name, desc) {
     this.name = name;
+    this._desc = desc || ''; // 数据描述，用于拼接规则 errorMessage 得到最终错误描述
     this._required = false;
     this._requiredMessage = '';
     this._trim = false;
     this._rules = [];
-    this._priorityRules = []; // Priority check rule
+    this._priorityRules = []; // 优先规则
   };
 
   Type.prototype.check = function check (value, data) {
     if (this._required && !checkRequired(value, this.trim)) {
-      return { hasError: true, errorMessage: this._requiredMessage }
+      return { hasError: true, errorMessage: this._desc + this._requiredMessage }
     }
 
-    var validator = createValidator(data);
+    var validator = createValidator(data, this._desc);
     var checkStatus = validator(value, this._priorityRules);
 
     if (checkStatus) {
@@ -78,11 +79,17 @@ var SchemaValidate = (function (exports) {
   };
 
   Type.prototype.required = function required (errorMessage, trim) {
+      if ( errorMessage === void 0 ) errorMessage = '不能为空';
       if ( trim === void 0 ) trim = true;
 
     this._required = true;
     this._trim = trim;
     this._requiredMessage = errorMessage;
+    return this
+  };
+
+  Type.prototype.desc = function desc (desc$1) {
+    this._desc = desc$1 || '';
     return this
   };
 
@@ -94,6 +101,7 @@ var SchemaValidate = (function (exports) {
     var n = {};
     Object.setPrototypeOf(n, Object.getPrototypeOf(t)); // 配置继承关系
     n.name = t.name;
+    n._desc = t._desc;
     n._required = t._required;
     n._requiredMessage = t._requiredMessage;
     n._trim = t._trim;
@@ -104,12 +112,17 @@ var SchemaValidate = (function (exports) {
 
   var Type_1 = Type;
 
-  var StringType = /*@__PURE__*/(function (Type) {
-    function StringType(errorMessage) {
-      if ( errorMessage === void 0 ) errorMessage = 'Please enter a valid string';
+  var REG_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // http://emailregex.com/
+  var REG_IP = /^((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}$/;
+  var REG_URL = new RegExp(
+    '^(?!mailto:)(?:(?:http|https|ftp)://|//)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$',
+    'i'
+  );
 
-      Type.call(this, 'string');
-      this.rule(function (v) { return typeof v === 'string'; }, errorMessage);
+  var StringType = /*@__PURE__*/(function (Type) {
+    function StringType(desc) {
+      Type.call(this, 'string', desc);
+      this.rule(function (v) { return typeof v === 'string'; }, '应为字符串');
     }
 
     if ( Type ) StringType.__proto__ = Type;
@@ -117,67 +130,94 @@ var SchemaValidate = (function (exports) {
     StringType.prototype.constructor = StringType;
 
     StringType.prototype.hasLetter = function hasLetter (errorMessage) {
-      return this.rule(function (v) { return /[a-zA-Z]/.test(v); }, errorMessage)
+      return this.rule(function (v) { return /[a-zA-Z]/.test(v); }, errorMessage || '应包含字母')
     };
 
     StringType.prototype.hasUppercaseLetter = function hasUppercaseLetter (errorMessage) {
-      return this.rule(function (v) { return /[A-Z]/.test(v); }, errorMessage)
+      return this.rule(function (v) { return /[A-Z]/.test(v); }, errorMessage || '应包含大写字母')
     };
 
     StringType.prototype.hasLowercaseLetter = function hasLowercaseLetter (errorMessage) {
-      return this.rule(function (v) { return /[a-z]/.test(v); }, errorMessage)
+      return this.rule(function (v) { return /[a-z]/.test(v); }, errorMessage || '应包含小写字母')
     };
 
     StringType.prototype.letterOnly = function letterOnly (errorMessage) {
-      return this.rule(function (v) { return /^[a-zA-Z]+$/.test(v); }, errorMessage)
+      return this.rule(function (v) { return /^[a-zA-Z]+$/.test(v); }, errorMessage || '只能是字母')
     };
 
     StringType.prototype.hasNumber = function hasNumber (errorMessage) {
-      return this.rule(function (v) { return /[0-9]/.test(v); }, errorMessage)
+      return this.rule(function (v) { return /[0-9]/.test(v); }, errorMessage || '应包含数字')
+    };
+
+    StringType.prototype.numberOnly = function numberOnly (errorMessage) {
+      return this.rule(function (v) { return /^\d+$/.test(v); }, errorMessage || '只能是数字')
+    };
+
+    StringType.prototype.isInteger = function isInteger (errorMessage)  {
+      return this.rule(function (v) { return /^-?\d+$/.test(v); }, errorMessage || '应为整数')
+    };
+
+    StringType.prototype.isNumber = function isNumber (errorMessage)  {
+      return this.rule(function (v) { return /^-?\d+(\.\d+)$/.test(v); }, errorMessage || '应为数值')
+    };
+
+    StringType.prototype.isFloat = function isFloat (errorMessage)  {
+      return this.isNumber(errorMessage || '应为 Float')
+    };
+
+    StringType.prototype.isDouble = function isDouble (errorMessage)  {
+      return this.isNumber(errorMessage || '应为 Double')
     };
 
     StringType.prototype.oneOf = function oneOf (list, errorMessage) {
-      return this.rule(function (v) { return list.indexOf(v) !== -1; }, errorMessage)
+      return this.rule(function (v) { return list.indexOf(v) !== -1; }, errorMessage || '非指定值')
     };
 
     StringType.prototype.email = function email (errorMessage) {
-      //http://emailregex.com/
-      var regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return this.rule(function (v) { return regexp.test(v); }, errorMessage)
+      return this.rule(function (v) { return REG_EMAIL.test(v); }, errorMessage || '应为 Email')
+    };
+
+    StringType.prototype.ip = function ip (errorMessage) {
+      return this.rule(function (v) { return REG_IP.test(v); }, errorMessage || '应为 IP 地址')
     };
 
     StringType.prototype.url = function url (errorMessage) {
-      var regexp = new RegExp(
-        '^(?!mailto:)(?:(?:http|https|ftp)://|//)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$',
-        'i'
-      );
-      return this.rule(function (v) { return regexp.test(v); }, errorMessage);
+      return this.rule(function (v) { return REG_URL.test(v); }, errorMessage || '应为 URL')
     };
 
     // 颜色值？例如 #fff #ffffff
     StringType.prototype.hex = function hex (errorMessage) {
-      var regexp = /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i;
-      return this.rule(function (v) { return regexp.test(v); }, errorMessage)
+      return this.rule(function (v) { return /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i.test(v); }, errorMessage || '应为十六进制颜色')
     };
 
-    StringType.prototype.pattern = function pattern (regexp, errorMessage) {
-      return this.rule(function (value) { return regexp.test(value); }, errorMessage)
+    StringType.prototype.pattern = function pattern (regex, errorMessage) {
+      return this.rule(function (value) { return regex.test(value); }, errorMessage || '格式错误')
     };
 
-    StringType.prototype.range = function range (minLength, maxLength, errorMessage) {
-      return this.rule(function (value) { return value.length >= minLength && value.length <= maxLength; }, errorMessage)
+    StringType.prototype.range = function range (min, max, errorMessage) {
+      return this.rule(
+        function (value) { return value.length >= min && value.length <= max; },
+        errorMessage || ("长度应在 " + min + " 到 " + max + " 字符之间")
+      )
     };
 
-    StringType.prototype.minlen = function minlen (minLength, errorMessage) {
-      return this.rule(function (value) { return value.length >= minLength; }, errorMessage)
+    StringType.prototype.minlen = function minlen (min, errorMessage) {
+      return this.rule(function (value) { return value.length >= min; }, errorMessage || ("长度不小于 " + min + " 字符"))
     };
 
-    StringType.prototype.maxlen = function maxlen (maxLength, errorMessage) {
-      return this.rule(function (value) { return value.length <= maxLength; }, errorMessage)
+    StringType.prototype.maxlen = function maxlen (max, errorMessage) {
+      return this.rule(function (value) { return value.length <= max; }, errorMessage || ("长度不大于 " + max + " 字符"))
+    };
+
+    StringType.prototype.length = function length (len, errorMessage) {
+      return this.rule(function (value) { return value.length === len; }, errorMessage || ("长度应为 " + len + " 字符"))
     };
 
     StringType.prototype.same = function same (field, errorMessage) {
-      return this.rule(function (value, data) { return data ? data[field] === value : false; }, errorMessage)
+      return this.rule(
+        function (value, data) { return data ? data[field] === value : false; },
+        errorMessage || ("值应与字段 " + field + " 一致")
+      )
     };
 
     return StringType;
@@ -259,11 +299,9 @@ var SchemaValidate = (function (exports) {
   };
 
   var NumberType = /*@__PURE__*/(function (Type) {
-    function NumberType(errorMessage) {
-      if ( errorMessage === void 0 ) errorMessage = 'Please enter a valid number';
-
-      Type.call(this, 'number');
-      this.rule(function (value) { return typeof value === 'number'; }, errorMessage);
+    function NumberType(desc) {
+      Type.call(this, 'number', desc);
+      this.rule(function (value) { return typeof value === 'number'; }, '应为数值');
     }
 
     if ( Type ) NumberType.__proto__ = Type;
@@ -271,23 +309,23 @@ var SchemaValidate = (function (exports) {
     NumberType.prototype.constructor = NumberType;
 
     NumberType.prototype.integer = function integer (errorMessage) {
-      return this.rule(function (value) { return value.toString().indexOf('.') === -1; }, errorMessage)
+      return this.rule(function (value) { return value.toString().indexOf('.') === -1; }, errorMessage || '应为整数')
     };
 
     NumberType.prototype.range = function range (min, max, errorMessage) {
-      return this.rule(function (value) { return value >= min && value <= max; }, errorMessage)
+      return this.rule(function (value) { return value >= min && value <= max; }, errorMessage || ("应在 " + min + " 到 " + max + " 之间"))
     };
 
     NumberType.prototype.min = function min (min$1, errorMessage) {
-      return this.rule(function (value) { return value >= min$1; }, errorMessage)
+      return this.rule(function (value) { return value >= min$1; }, errorMessage || ("应不小于 " + min$1))
     };
 
     NumberType.prototype.max = function max (max$1, errorMessage) {
-      return this.rule(function (value) { return value <= max$1; }, errorMessage)
+      return this.rule(function (value) { return value <= max$1; }, errorMessage || ("应不大于 " + max$1))
     };
 
     NumberType.prototype.oneOf = function oneOf (list, errorMessage) {
-      return this.rule(function (value) { return list.indexOf(value) !== -1; }, errorMessage)
+      return this.rule(function (value) { return list.indexOf(value) !== -1; }, errorMessage || '非指定值')
     };
 
     return NumberType;
@@ -296,11 +334,9 @@ var SchemaValidate = (function (exports) {
   var NumberType_1 = function (errorMessage) { return new NumberType(errorMessage); };
 
   var BooleanType = /*@__PURE__*/(function (Type) {
-    function BooleanType(errorMessage) {
-      if ( errorMessage === void 0 ) errorMessage = 'Please enter a valid `boolean`';
-
-      Type.call(this, 'boolean');
-      this.rule(function (v) { return typeof v === 'boolean'; }, errorMessage);
+    function BooleanType(desc) {
+      Type.call(this, 'boolean', desc);
+      this.rule(function (v) { return typeof v === 'boolean'; }, '应为布尔值');
     }
 
     if ( Type ) BooleanType.__proto__ = Type;
@@ -313,11 +349,9 @@ var SchemaValidate = (function (exports) {
   var BooleanType_1 = function (errorMessage) { return new BooleanType(errorMessage); };
 
   var DateType = /*@__PURE__*/(function (Type) {
-    function DateType(errorMessage) {
-      if ( errorMessage === void 0 ) errorMessage = 'Please enter a valid date';
-
-      Type.call(this, 'date');
-      this.rule(function (value) { return !/Invalid|NaN/.test(new Date(value)); }, errorMessage);
+    function DateType(desc) {
+      Type.call(this, 'date', desc);
+      this.rule(function (value) { return !/Invalid|NaN/.test(new Date(value)); }, '应为日期');
     }
 
     if ( Type ) DateType.__proto__ = Type;
@@ -327,20 +361,20 @@ var SchemaValidate = (function (exports) {
     DateType.prototype.range = function range (min, max, errorMessage) {
       return this.rule(
         function (value) { return new Date(value) >= new Date(min) && new Date(value) <= new Date(max); },
-        errorMessage
+        errorMessage || ("应在 " + (min.toString()) + " 到 " + (max.toString()) + " 之间")
       )
     };
 
     DateType.prototype.min = function min (min$1, errorMessage) {
-      return this.rule(function (value) { return new Date(value) >= new Date(min$1); }, errorMessage)
+      return this.rule(function (value) { return new Date(value) >= new Date(min$1); }, errorMessage || ("应不小于 " + (min$1.toString())))
     };
 
     DateType.prototype.max = function max (max$1, errorMessage) {
-      return this.rule(function (value) { return new Date(value) <= new Date(max$1); }, errorMessage)
+      return this.rule(function (value) { return new Date(value) <= new Date(max$1); }, errorMessage || ("应不大于 " + (max$1.toString())))
     };
 
     DateType.prototype.strict = function strict (errorMessage) {
-      return this.rule(function (value) { return Object.prototype.toString.call(value) === '[object Date]'; }, errorMessage)
+      return this.rule(function (value) { return Object.prototype.toString.call(value) === '[object Date]'; }, errorMessage || '应为日期类型')
     };
 
     return DateType;
@@ -349,11 +383,9 @@ var SchemaValidate = (function (exports) {
   var DateType_1 = function (errorMessage) { return new DateType(errorMessage); };
 
   var ObjectType = /*@__PURE__*/(function (Type) {
-    function ObjectType(errorMessage) {
-      if ( errorMessage === void 0 ) errorMessage = 'Please enter a valid `object`';
-
-      Type.call(this, 'object');
-      this.rule(function (v) { return typeof v === 'object'; }, errorMessage);
+    function ObjectType(desc) {
+      Type.call(this, 'object', desc);
+      this.rule(function (v) { return typeof v === 'object'; }, '应为对象');
     }
 
     if ( Type ) ObjectType.__proto__ = Type;
@@ -391,11 +423,9 @@ var SchemaValidate = (function (exports) {
   var ObjectType_1 = function (errorMessage) { return new ObjectType(errorMessage); };
 
   var ArrayType = /*@__PURE__*/(function (Type) {
-    function ArrayType(errorMessage) {
-      if ( errorMessage === void 0 ) errorMessage = 'Please enter a valid array';
-
-      Type.call(this, 'array');
-      this.rule(function (v) { return Array.isArray(v); }, errorMessage);
+    function ArrayType(desc) {
+      Type.call(this, 'array', desc);
+      this.rule(function (v) { return Array.isArray(v); }, '应为数组');
     }
 
     if ( Type ) ArrayType.__proto__ = Type;
@@ -403,15 +433,22 @@ var SchemaValidate = (function (exports) {
     ArrayType.prototype.constructor = ArrayType;
 
     ArrayType.prototype.range = function range (min, max, errorMessage) {
-      return this.rule(function (value) { return value.length >= min && value.length <= max; }, errorMessage)
+      return this.rule(
+        function (value) { return value.length >= min && value.length <= max; },
+        errorMessage || ("长度应在 " + min + " 到 " + max + " 之间")
+      )
     };
 
     ArrayType.prototype.minlen = function minlen (min, errorMessage) {
-      return this.rule(function (value) { return value.length >= min; }, errorMessage)
+      return this.rule(function (value) { return value.length >= min; }, errorMessage || ("长度不小于 " + min))
     };
 
-    ArrayType.prototype.maxlen = function maxlen (maxLength, errorMessage) {
-      return this.rule(function (value) { return value.length <= maxLength; }, errorMessage)
+    ArrayType.prototype.maxlen = function maxlen (max, errorMessage) {
+      return this.rule(function (value) { return value.length <= max; }, errorMessage || ("长度不大于 " + max))
+    };
+
+    ArrayType.prototype.length = function length (len, errorMessage) {
+      return this.rule(function (value) { return value.length === len; }, errorMessage || ("长度应为 " + len))
     };
 
     ArrayType.prototype.unique = function unique (errorMessage) {
@@ -426,7 +463,7 @@ var SchemaValidate = (function (exports) {
           hash[i] = true;
         }
         return true
-      }, errorMessage)
+      }, errorMessage || '不能重复')
     };
 
     ArrayType.prototype.uniqueKey = function uniqueKey (key, errorMessage) {
@@ -442,17 +479,16 @@ var SchemaValidate = (function (exports) {
           hash[v] = true;
         }
         return true
-      }, errorMessage)
+      }, errorMessage || '不能重复')
     };
 
     /**
      * @example
      * ArrayType('这是一个数组').of(
-     *      StringType().isOneOf(['数码','体育','游戏','旅途','其他'],
-     *      '只能是选择中的值'
+     *   StringType().isOneOf(['数码','体育','游戏','旅途','其他']
      * )
      */
-    ArrayType.prototype.of = function of (type, errorMessage) {
+    ArrayType.prototype.of = function of (type) {
       return this.rule(function (items, data) {
         for (var i = 0, list = items; i < list.length; i += 1) {
           var item = list[i];
@@ -461,7 +497,7 @@ var SchemaValidate = (function (exports) {
           if (result.hasError) { return result }
         }
         return true
-      }, errorMessage)
+      })
     };
 
     return ArrayType;
